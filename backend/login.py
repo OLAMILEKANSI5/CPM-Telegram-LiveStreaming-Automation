3453f270b500d9d3c0717397dc528a35f46e8d7c
JAMES OLALEKAN SAMUEL <66016793+OLAMILEKANSI5@users.noreply.github.com>
2026-07-14 23:09:07 +0100

    Add files via upload

--- backend/login.py
+++ backend/login.py
@@ -12,6 +12,12 @@ Usage:
     python login.py
 """
 import asyncio
+import platform
+
+if platform.system() == "Windows":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
 from pyrogram import Client
 from app.config import settings
 from app import db
